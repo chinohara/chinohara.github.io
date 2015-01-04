3b3c5f8570837a2b0d59b0ff05314414fc36b732
snhr.not@gmail.com <snhr.not@gmail.com>
2015-01-05 01:04:23 +0900

    Automated commit at 2015-01-04 16:04:23 UTC by middleman-deploy 1.0.0

---
+++ javascripts/common.js
@@ -0,0 +1,14 @@
+// $(document).ready(function() {
+// $('#simple-menu').sidr();
+// });
+
+$(document).ready(function() {
+$('#left-menu').sidr({
+name: 'sidr-left',
+side: 'left' // By default
+});
+$('#right-menu').sidr({
+name: 'sidr-right',
+side: 'right'
+});
+});
